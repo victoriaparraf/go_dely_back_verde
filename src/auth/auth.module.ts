17c1ea71b5f1f6eac160ad453b892f19f5f3aa19
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2024-11-28 10:52:00 -0400

    feat: :sparkles: jwt strategy and passport added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './application/auth.controller';
-import { AuthService } from './application/auth.service';
+import { AuthController } from './infrastructure/auth.controller';
+import { AuthService } from './infrastructure/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './domain/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './infrastructure/jwt/strategies/jwt.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([ User ])],
-  exports: [ TypeOrmModule ]
+  controllers: [ AuthController ],
+  providers: [ AuthService, JwtStrategy ],
+  imports: [
+
+    ConfigModule,
+    
+    TypeOrmModule.forFeature([ User ]),
+
+    PassportModule.register( { defaultStrategy: 'jwt' } ),
+
+    JwtModule.registerAsync({
+
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2h'
+        }
+      })
+    })
+
+  ],
+  exports: [ TypeOrmModule, JwtStrategy, PassportModule, JwtModule ]
 })
 export class AuthModule {}
--- src/auth/infrastructure/auth.controller.ts
+++ src/auth/infrastructure/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../infrastructure/dto/create-user.dto';
 import { LoginUserDto } from '../infrastructure/dto/login-user.dto';
