968eee89959e00bbb93bbcd0859409787b340083
victoriaparraf <victoriaparrafranco@gmail.com>
2025-01-05 16:53:48 -0400

    feat: ✨ delete combo service

---
+++ src/combo/application/commands/delete-combo.service.ts
@@ -0,0 +1,24 @@
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ComboRepository } from '../../infrastructure/repositories/combo-repository';
+
+export class DeleteComboService{
+    constructor(
+        private readonly comboRepository: ComboRepository
+    ){}
+
+    async execute(comboID: string): Promise<void> {
+
+        const combo = await this.comboRepository.findOne(comboID);
+        if(!combo){
+            throw new NotFoundException(`Product with ID ${comboID} not found`)
+        }
+
+        try {
+            await this.comboRepository.removeCombo(combo);
+        } catch (error) {
+            console.error('Error deleting product:', error);
+            throw new InternalServerErrorException('Unexpected error, check server logs');
+        }
+        
+    }
+}
