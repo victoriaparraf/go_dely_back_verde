8fd21d50c9443f379d0235a8ec89f8476694e049
sofiaarasme <111990944+sofiaarasme@users.noreply.github.com>
2025-01-16 23:29:52 -0400

    fix: :bug: order items mapper fixed

--- src/order/infraestructure/mappers/order.mapper.ts
+++ src/order/infraestructure/mappers/order.mapper.ts
@@ -18,29 +18,36 @@ export class OrderMapper {
         paymentMethod: order.getPaymentMethodName().value,
         status: order.getStatus(),
         cupon_code: order.getCupon()?.value,
-        order_products: order.getOrderProducts().map(product => ({
-            order_id: product.order_id,
-            product_id: product.product_id,
-            quantity: product.quantity,
-            product_price: product.product_price,
-            total_price: product.total_price,
-            product: product.product
-        })),
+        order_products: order.getOrderProducts().map(product => {
+          if (!product.product) {
+              throw new Error(`Product with ID ${product.product_id} not found`);
+          }
+          return {
+              id: product.product_id,
+              quantity: product.quantity,
+              price: product.product_price,
+              total_price: product.total_price,
+              name: product.product.product_name.getValue(),
+              description: product.product.product_description.getValue(),
+              currency: product.product.product_currency.getValue(),
+              stock: product.product.product_stock.getValue(),
+              weight: product.product.product_weight.getValue()
+          };
+      }),
         order_combos: order.getOrderCombos().map(combo => {
             if (!combo.combo) {
                 throw new Error(`Combo with ID ${combo.combo_id} not found`);
             }
             return {
-                order_id: combo.order_id,
-                combo_id: combo.combo_id,
+                id: combo.combo_id,
                 quantity: combo.quantity,
-                combo_price: combo.combo_price,
+                price: combo.combo_price,
                 total_price: combo.total_price,
-                combo_name: combo.combo.combo_name.getValue(),
-                combo_description: combo.combo.combo_description.getValue(),
-                combo_currency: combo.combo.combo_currency.getValue(),
-                combo_stock: combo.combo.combo_stock.getValue(),
-                combo_weight: combo.combo.combo_weight.getValue()
+                name: combo.combo.combo_name.getValue(),
+                description: combo.combo.combo_description.getValue(),
+                currency: combo.combo.combo_currency.getValue(),
+                stock: combo.combo.combo_stock.getValue(),
+                weight: combo.combo.combo_weight.getValue()
             };
         }),
     };
@@ -79,29 +86,27 @@ export class OrderMapper {
         status: order.getStatus(),
         cupon_code: order.getCupon()?.value,
         products: order.getOrderProducts().map(product => ({
-            id: product.order_id,
             quantity: product.quantity,
-            product_id: product.product_id,
-            product_price: product.product.product_price.getValue(),
+            id: product.product_id,
+            price: product.product.product_price.getValue(),
             total_price: product.total_price,
-            product_name: product.product.product_name.getValue(),
-            product_description: product.product.product_description.getValue(),
-            product_currency: product.product.product_currency.getValue(),
-            product_weight: product.product.product_weight.getValue(),
-            product_measurement: product.product.product_measurement.getValue(),
-            product_stock: product.product.product_stock.getValue()
+            name: product.product.product_name.getValue(),
+            description: product.product.product_description.getValue(),
+            currency: product.product.product_currency.getValue(),
+            weight: product.product.product_weight.getValue(),
+            measurement: product.product.product_measurement.getValue(),
+            stock: product.product.product_stock.getValue()
         })),
         combos: order.getOrderCombos().map(combo => ({
-            id: combo.order_id,
             quantity: combo.quantity,
-            combo_id: combo.combo_id,
-            combo_price: combo.combo.combo_price.getValue(),
+            id: combo.combo_id,
+            price: combo.combo.combo_price.getValue(),
             total_price: combo.total_price,
-            combo_name: combo.combo.combo_name.getValue(),
-            combo_description: combo.combo.combo_description.getValue(),
-            combo_currency: combo.combo.combo_currency.getValue(),
-            combo_stock: combo.combo.combo_stock.getValue(),
-            combo_weight: combo.combo.combo_weight.getValue()
+            name: combo.combo.combo_name.getValue(),
+            description: combo.combo.combo_description.getValue(),
+            currency: combo.combo.combo_currency.getValue(),
+            stock: combo.combo.combo_stock.getValue(),
+            weight: combo.combo.combo_weight.getValue()
         })),
     };
   }
